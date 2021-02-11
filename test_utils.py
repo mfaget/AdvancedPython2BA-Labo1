2bd16fe9db2e01360127c14d3d1995f7883fe29c
Matthieu Faget <20517@ecam.be>
2021-02-11 21:59:54 +0100

    test travis2

--- test_utils.py
+++ test_utils.py
@@ -7,7 +7,7 @@ import utils
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
-        self.assertEqual(utils.fact(5) , 0)
+        self.assertEqual(utils.fact(0) , 0)
         pass
     
     def test_roots(self):
